630d81aefa1801ac3004b9222f4e4959207323e0
bikramsk <bikramkanyal@gmail.com>
2025-06-03 17:43:14 +0530

    Update production server config

--- config/server.js
+++ config/server.js
@@ -1,13 +1,29 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://gahoishakti.in'), 
+  host: '0.0.0.0', // Required for Render
+  port: env.int('PORT', 1337), 
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'), 
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  admin: {
+    url: '/admin', 
+    serveAdminPanel: true, 
   },
 });
+
+
+
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
 
 
 
